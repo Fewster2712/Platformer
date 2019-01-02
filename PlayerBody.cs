b2f7359cc0baea202b353dbefce9f138a72b52e9
Fewster2712 <oliverws2712@gmail.com>
2019-01-02 10:55:54 +0100

    Movement almost complete

--- PlayerBody.cs
+++ PlayerBody.cs
@@ -3,30 +3,35 @@ using System;
 
 public class PlayerBody : KinematicBody2D
 {
+    //Movement
     const int speed = 10;
     Vector2 floor = new Vector2(0,-1);
     const int gravity = 15;
-    const int maxspeed = 60;
+    const int maxspeed = 140;
     const int minspeed = 10;
-    const int jumppower = -250;
-    bool on_ground = true;
+    const int jumppower = -350;
+    const int deacl = 5;
+    const int range = 25;
+    bool onFloor = false;
     Vector2 velocity;
-    
+    //Movement ended
     
 
     public override void _PhysicsProcess(float delta)
     {
-     
-        if(Input.IsKeyPressed((int) KeyList.A))
+        //Movement
+        var anim = (AnimatedSprite)GetNode("Playersprite");
+        if (Input.IsKeyPressed((int) KeyList.A))
         {
           
           velocity.x -= speed;
-            if(velocity.x > maxspeed*-1)
+
+            if (velocity.x < maxspeed*-1)
             {
                 velocity.x = maxspeed*-1;
             }
         }   
-          if(Input.IsKeyPressed((int) KeyList.D))
+         if(Input.IsKeyPressed((int) KeyList.D))
         {
             velocity.x += speed;
             if (velocity.x > maxspeed)
@@ -37,26 +42,62 @@ public class PlayerBody : KinematicBody2D
         }
         if(!Input.IsKeyPressed((int) KeyList.D) && !Input.IsKeyPressed((int) KeyList.A))
         {
-            velocity.x = 0;
+            if(velocity.x > 0)
+            {
+                velocity.x -= deacl;
+            }
+            else if(velocity.x < 0)
+            {
+                velocity.x += deacl;
+            }
+            anim.Play("idle");
         }
         
           if(Input.IsKeyPressed((int) KeyList.W))
         {
-            if (on_ground = true)
+            if(onFloor == true)
             {
+                anim.Play("jump");
             velocity.y = jumppower;
             }
         }
-        
+        else if(velocity.y > 0)
+        {
+            anim.Play("fall");
+        }
+        if(velocity.x > 0)
+        {
+            anim.Play("run");
+            anim.FlipH = false;
+        }
+        else if(velocity.x < 0)
+        {
+            anim.Play("run");
+            anim.FlipH = true;
+        }
         velocity.y += gravity;
         velocity = MoveAndSlide(velocity, floor);
+        if(this.IsOnFloor() == true)
+        {
+            onFloor = true;
+        }
+        else 
+        {
+        onFloor = false;
+        }
+        //Movement ended
+
+        //Attack
+        var root = GetTree().GetRoot();
+        var enemy = (Node2D)root.GetNode("Enemy");
+        enemy.
         
     }
 
-//    public override void _Process(float delta)
+//    public o)verride void _Process(float delta)
 //    {
 //        // Called every frame. Delta is time since last frame.
-//        // Update game logic here.
+//            // Update game logic here.
 //        
 //    }
 }
